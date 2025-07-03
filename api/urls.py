c50976996c9a460b0411970a730cf8ccf84af80b
Ruan Batista <145821191+zzruanxx@users.noreply.github.com>
2025-07-03 02:27:11 +0000

    Issue1 - Concluida

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 # api/urls.py
 
-from .views import EpisodeList
+from .views import EpisodeList, EpisodeDetail
 from django.urls import path
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', EpisodeList.as_view(), name='episode-list'),
 
     # Rota para um episódio específico, ex: /api/episodes/1/
-    # path('<int:pk>/', EpisodeDetail.as_view(), name='episode-detail'),  # Remova ou comente esta linha
+    path('<str:id>/', EpisodeDetail.as_view(), name='episode-detail'),
 
     # Rota para o endpoint de status: /api/episodes/status/
     # path('status/', cosmosdb_status),  # Remova ou comente esta linha, pois cosmosdb_status não está definido
--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .cosmosdb_connection import listar_episodios, criar_episodio
+from .cosmosdb_connection import listar_episodios, criar_episodio, ler_item
 
 class EpisodeList(APIView):
     """
@@ -17,3 +17,14 @@ class EpisodeList(APIView):
         episodio = request.data
         result = criar_episodio(episodio)
         return Response(result, status=status.HTTP_201_CREATED)
+
+class EpisodeDetail(APIView):
+    """
+    Retrieve, update or delete a episode instance.
+    """
+    def get(self, request, id):
+        try:
+            episodio = ler_item(item_id=id, pk=id)
+            return Response(episodio)
+        except Exception:
+            return Response({"detail": "Episódio não encontrado."}, status=status.HTTP_404_NOT_FOUND)
